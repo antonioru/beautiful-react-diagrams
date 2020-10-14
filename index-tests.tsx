0d045e1c3658110707c65ffc3c2c5953f0ffe054
Keisuke Kan <keisuke_kan@caddi.jp>
2020-10-14 11:31:44 +0900

    refactor: format

--- index-tests.tsx
+++ index-tests.tsx
@@ -1,25 +1,25 @@
-import * as React from "react";
-import Diagram, { useSchema } from ".";
-import { Button } from "beautiful-react-ui";
+import * as React from 'react';
+import Diagram, { useSchema } from '.';
+import { Button } from 'beautiful-react-ui';
 
 export const UncontrolledDiagram1 = () => {
   // create diagrams schema
   const [schema, { onChange }] = useSchema({
     nodes: [
-      { id: "node-1", content: "Node 1", coordinates: [250, 60] },
-      { id: "node-2", content: "Node 2", coordinates: [100, 200] },
-      { id: "node-3", content: "Node 3", coordinates: [250, 220] },
-      { id: "node-4", content: "Node 4", coordinates: [400, 200] },
+      { id: 'node-1', content: 'Node 1', coordinates: [250, 60] },
+      { id: 'node-2', content: 'Node 2', coordinates: [100, 200] },
+      { id: 'node-3', content: 'Node 3', coordinates: [250, 220] },
+      { id: 'node-4', content: 'Node 4', coordinates: [400, 200] },
     ],
     links: [
-      { input: "node-1", output: "node-2" },
-      { input: "node-1", output: "node-3" },
-      { input: "node-1", output: "node-4" },
+      { input: 'node-1', output: 'node-2' },
+      { input: 'node-1', output: 'node-3' },
+      { input: 'node-1', output: 'node-4' },
     ],
   });
 
   return (
-    <div style={{ height: "22.5rem" }}>
+    <div style={{ height: '22.5rem' }}>
       <Diagram schema={schema} onChange={onChange} />
     </div>
   );
@@ -35,8 +35,8 @@ const CustomRender: React.FC<{
 }> = ({ content, data }) => (
   <div
     onDoubleClick={data.onDoubleClick}
-    role="button"
-    style={{ padding: "15px", background: "purple" }}
+    role='button'
+    style={{ padding: '15px', background: 'purple' }}
   >
     {content}
   </div>
@@ -46,10 +46,10 @@ export const UncontrolledDiagram2 = () => {
   const [schema, { onChange, addNode, removeNode }] = useSchema<CustomData>({
     nodes: [
       {
-        id: "node-1",
-        content: "Node 1",
+        id: 'node-1',
+        content: 'Node 1',
         coordinates: [150, 60],
-        outputs: [{ id: "port-1", alignment: "right" }],
+        outputs: [{ id: 'port-1', alignment: 'right' }],
       },
     ],
   });
@@ -75,11 +75,11 @@ export const UncontrolledDiagram2 = () => {
   };
 
   return (
-    <div style={{ height: "22.5rem" }}>
-      <Button color="primary" icon="plus" onClick={addNewNode}>
+    <div style={{ height: '22.5rem' }}>
+      <Button color='primary' icon='plus' onClick={addNewNode}>
         Add new node
       </Button>
-      <Button color="secondary" icon="minus" onClick={removeLast}>
+      <Button color='secondary' icon='minus' onClick={removeLast}>
         Remove last node
       </Button>
       <Diagram schema={schema} onChange={onChange} />
