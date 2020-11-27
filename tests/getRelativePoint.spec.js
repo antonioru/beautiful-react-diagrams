cd69f30a9760024d7a3265b462ee605d963c7fa3
Joan Cejudo <joancc@gmail.com>
2020-11-27 18:23:45 -0500

    Update unit test for relative point

--- tests/getRelativePoint.spec.js
+++ tests/getRelativePoint.spec.js
@@ -6,6 +6,6 @@ describe('getRelativePoint utility function', () => {
   });
 
   it('should calculate a point coordinates relatively to another', () => {
-    expect(getRelativePoint([10, 10], [20, 20])).to.deep.equal([-10, -10]);
+    expect(getRelativePoint({ x: 10, y: 10 }, { x: 20, y: 20 })).to.deep.equal([-10, -10]);
   });
 });
